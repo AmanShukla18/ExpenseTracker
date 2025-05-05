7db36dc5f475e7931e28a03ecba0f885a8714974
Aman Shukla <aman.a.shukla183@gmail.com>
2025-05-05 17:19:55 +0530

    Done

---
+++ webpack.config.client.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './client/main.js', 
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'assets/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
